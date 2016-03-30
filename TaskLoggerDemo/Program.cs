ee1c25e0c2cd38315a57ab255f83e0671447fe83
sarapinit <sarapinit@ya.ru>
2016-03-30 23:28:04 +0600

    -initial commit

---
+++ TaskLoggerDemo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskLoggerDemo
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+#if DEBUG
+      TaskLogger.LogLevel = TaskLogger.TaskLogLevel.Pending;
+#endif
+      var tasks = new List<Task>
+      {
+        Task.Delay(2000).Log("2s op"),
+        Task.Delay(5000).Log("5s op"),
+        Task.Delay(6000).Log("6s op")
+      };
+      try
+      {
+         //(await Task.WhenAll(tasks))
+      }
+      catch (OperationCanceledException)
+      {
+        foreach (var op in TaskLogger.GetLogEntries().OrderBy(t => t.LogTime))
+          Console.WriteLine(op);
+      }
+    }
+  }
+}
